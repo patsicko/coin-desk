3327e7498161b20ca3f81c75f9e1e8f1869eebad
patsicko <patsicko@g>
2022-07-19 22:56:31 +0200

    Feat:coin-base

--- index.js
+++ index.js
@@ -25,38 +25,14 @@ const cardDisplay=(time,code,rate,description)=>{
   </div> 
     </div>
     `
-
 main.innerHTML+=cards;
 
-
 }
-
-
-let updated;
-let code;
-let rate;
-let description;
-
 
 const fetchData=async()=>{
     const data=await (await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")).json();
     console.log(data);
    
-
-     updated=data.time.updated;
-     code=data.bpi.USD.code;
-     rate=data.bpi.USD.rate;
-     description=data.bpi.USD.description;
-
-     
-     code=data.bpi.GBP.code;
-     rate=data.bpi.GBP.rate;
-     description=data.bpi.GBP.description;
-
-   
-     code=data.bpi.EUR.code;
-     rate=data.bpi.EUR.rate;
-     description=data.bpi.EUR.description;
 
      cardDisplay(data.time.updated,data.bpi.USD.code,data.bpi.USD.rate,data.bpi.USD.description);
      cardDisplay(data.time.updated,data.bpi.GBP.code,data.bpi.GBP.rate,data.bpi.GBP.description);
